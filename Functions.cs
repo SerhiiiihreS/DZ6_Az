2476fd995e3625bc5195a28012e93a86ed38369f
SerhiiiihreS <0992830717s@gmail.com>
2025-11-04 21:29:57 +0100

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mime;
+using System.Reflection.Metadata;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+namespace DZ6_Az
+{
+    public class Functions
+    {
+        /*
+        public void Proccess([QueueTrigger("images")] string message, ILogger logger, 
+            [Blob("big-images/{queueTrigger}", FileAccess.Read)] Stream inputImage,
+            [Blob("small-images/grayscale-{queueTrigger}", FileAccess.Write)] Stream outputImage)
+        {
+            logger.LogWarning($"Image with name {message} was read, Lenght: {inputImage.Length} bytes");
+            using (Image image = Image.Load(inputImage))
+            {
+                image.Mutate(x => x.Grayscale(0.8f));
+                image.Save(outputImage, new PngEncoder());
+                logger.LogWarning($"Image with name {message} was proccessed, Lenght: {inputImage.Length} bytes");
+            }
+        }
+        */
+
+
+
+        public void Proccess([QueueTrigger("images")] string message, ILogger logger,
+            [Blob("original-images/{queueTrigger}", FileAccess.Read)] Stream inputImage,
+            [Blob("small-images/{queueTrigger}", FileAccess.Write)] Stream outputImage)
+        {
+            //logger.LogWarning($"Image with name {message} was read, Lenght: {inputImage.Length} bytes");
+            using (Image image = Image.Load(inputImage))
+            {
+                image.Mutate(x => x.Resize(200, 100));
+                image.Save(outputImage, new PngEncoder());
+                //logger.LogWarning($"Image with name {message} was proccessed, Lenght: {inputImage.Length} bytes");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostBuilder builder = new HostBuilder();
+builder.ConfigureWebJobs(options =>
+{
+    options.AddAzureStorageBlobs();
+    options.AddAzureStorageQueues();
+});
+
+builder.ConfigureLogging((context, logBuilder) =>
+{
+    string instrmentationKey = context.Configuration
+                                   .GetValue<string>("AppInsights_InstrumentationKey")
+                               ?? throw new InvalidOperationException("You should provide instrumentation key");
+    logBuilder.AddConsole();
+    logBuilder.AddApplicationInsightsWebJobs(options =>
+        options.InstrumentationKey = instrmentationKey);
+});
+
+IHost host = builder.Build();
+using (host)
+    await host.RunAsync();
